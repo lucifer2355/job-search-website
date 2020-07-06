f762481e505898c3e75d1cd76616f6ecd5edd824
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-06 19:58:08 +0530

    add project

---
+++ src/Component/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className='header-title'>
+          <h1>Search Your Dream Job</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
